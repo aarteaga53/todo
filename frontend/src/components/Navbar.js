1fdfd32e6cc7e1770c756e6b51ce83299dd04249
AnaEstrada1996 <ana_estrada1@yahoo.com>
2023-02-03 12:18:39 -0800

    Added a navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom';
+import '../styles/Navbar.css'
+import ReorderIcon from '@mui/icons-material/Reorder';
+import { Button } from '@mui/material';
+
+function Navbar() {
+
+    const [openLinks, setOpenLinks] = useState(false)
+
+    const toggleNavbar = () =>{
+        setOpenLinks(!openLinks)
+    };
+
+  return (
+    <div className='navbar'>
+      <div className='leftSide' id={openLinks ? 'open' : 'close'}>
+            <div className='hiddenLinks'>
+            <Link to='/'>Home</Link>
+            <Link to='profile'>Profile</Link>
+            <Link to='tracker'>Issue Tracker</Link>
+            <Link to='contact'>Contact</Link>
+            </div>
+        </div>
+        <div className='rightSide'>
+            <Link to='/'>Home</Link>
+            <Link to='profile'>Profile</Link>
+            <Link to='tracker'>IssueTracker</Link>
+            <Link to='contact'>Contact</Link>
+            <Button onClick={toggleNavbar}>
+                <ReorderIcon/>
+            </Button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
