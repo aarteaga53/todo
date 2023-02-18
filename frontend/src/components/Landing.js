5cdf0bf5bec1b8213ec5b12616015004fa37d551
aarteaga53 <andrewarteaga123@gmail.com>
2023-02-17 18:17:25 -0800

    post it page curve, landing page updates

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -2,15 +2,33 @@ import React from 'react'
 import '../styles/Landing.css'
 
 const Landing = () => {
+  // left: Math.floor(Math.random() * (window.innerWidth - 275)), 
+  // top: Math.floor(Math.random() * (window.innerHeight - 555))
   const postIts = [
-    {color: 'gray', title: 'Tasks', body: 'Write out your tasks, activites, or appointments to keep track of.', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'yellow', title: 'Organize', body: 'Keep your tasks organized and easy to find.', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-green', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-blue', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-orange', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-purple', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-pink', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))},
-    {color: 'p-yellow', title: 'Title', body: 'Body', width: Math.floor(Math.random() * (window.innerWidth - 275)), height: Math.floor(Math.random() * (window.innerHeight - 325))}
+    {
+      color: 'p-green', title: 'Tasks', 
+      body: 'Write out your tasks, activities, or appointments to keep track of.'
+    },
+    {
+      color: 'p-blue', title: 'Organize', 
+      body: 'Keep your tasks organized and easy to find.'
+    },
+    {
+      color: 'p-orange', title: 'Color', 
+      body: 'Color code your tasks however you like.'
+    },
+    {
+      color: 'p-purple', title: 'Title', 
+      body: 'Body'
+    },
+    {
+      color: 'p-pink', title: 'Title', 
+      body: 'Body'
+    },
+    {
+      color: 'p-yellow', title: 'Title', 
+      body: 'Body'
+    }
   ]
 
   return (
@@ -21,19 +39,12 @@ const Landing = () => {
       </div>
       <div className='body'>
         {postIts.map((postIt, index) => (
-          <div className={`post-it ${postIt.color}`} style={{marginLeft: postIt.width, marginTop: postIt.height}} key={index}>
-            <div className='post-title'>{postIt.title}</div>
-            <div className='post-body'>{postIt.body}</div>
+          <div className={`post-it ${postIt.color}`} style={{marginTop: index * 100 + 50}} key={index}>
+            <div className={`corner ${postIt.color}-dark`}></div>
+            <input className='post-title post-select' defaultValue={postIt.title} />
+            <textarea className='post-body post-select' rows={20} defaultValue={postIt.body} />
           </div>
         ))}
-        {/* <div className='post-it gray'></div>
-        <div className='post-it yellow'></div>
-        <div className='post-it p-green'></div>
-        <div className='post-it p-blue'></div>
-        <div className='post-it p-orange'></div>
-        <div className='post-it p-purple'></div>
-        <div className='post-it p-pink'></div>
-        <div className='post-it p-yellow'></div> */}
       </div>
     </div>
   )
